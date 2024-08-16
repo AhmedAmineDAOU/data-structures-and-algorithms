15533a522aa97a45b4a39f044ec632a44a59e7b3
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2024-08-16 17:54:31 +0200

    simple implementation of SinglyLinkedList with int node data

--- src/main/java/lists/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/lists/singlylinkedlist/SinglyLinkedList.java
@@ -1,4 +1,25 @@
 package lists.singlylinkedlist;
 
 public class SinglyLinkedList {
+//    Singly Linked List
+//
+//    Singly Linked List is a data structure used for storing collection
+//    of nodes and has following properties -
+//
+//            • It contains sequence of nodes.
+//            • A node has data and reference to next node in a list.
+//            • First node is the head node.
+//            • Last node has data and points to null.
+
+    private ListNode head;
+
+    private static class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
 }
